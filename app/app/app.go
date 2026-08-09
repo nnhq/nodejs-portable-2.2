05a245a8d262d14d94e5028284c10abb32497aac
heqiao <41105170+nnhq@users.noreply.github.com>
2026-08-09 14:50:03 +0800

    Update app.go

--- app/app/app.go
+++ app/app/app.go
@@ -124,10 +124,11 @@ SET npmGlobalConfigFilePath=%npmPath%\npmrc
 
 SET PATH=%nodejsPath%;%PATH%
 cd "%nodejsWork%"
-call "%nodejsPath%\nodevars.bat"
-call "%nodejsPath%\npm.cmd" config set globalconfig "%npmGlobalConfigFilePath%" --global
-
 @INIT_COMMANDS@`
+"%nodejsPath%\nodevars.bat"
+"%nodejsPath%\npm.cmd" config set globalconfig "%npmGlobalConfigFilePath%" --global
+
+
 
 	launchScript := strings.Replace(launchScriptTpl, "@WORK_PATH@", workPath, -1)
 	launchScript = strings.Replace(launchScript, "@NODEJS_PATH@", nodePath, -1)
